c8b7b824a00f1cacfc55b43c5790a2633eb3c6f4
Xiaobo Gu <xiaobo.gu@amlogic.com>
2015-04-21 14:25:12 +0800

    PD#105858:Update multi device tree function
    Change-Id: I06b7b8b9c4e844e546421cd1a247207b3b81b090
    
    Conflicts:
    
    	common/aml_dt.c

--- common/aml_dt.c
+++ common/aml_dt.c
@@ -40,7 +40,7 @@ unsigned int get_multi_dt_entry(unsigned int fdt_addr){
 
 		/*version control, compatible with v1*/
 		dt_tool_version = readl(fdt_addr + AML_DT_VERSION_OFFSET);
-		unsigned int aml_each_id_length;
+		unsigned int aml_each_id_length=0;
 		unsigned int aml_dtb_offset_offset;
 		unsigned int aml_dtb_header_size;
 		if(dt_tool_version == 1)
@@ -58,17 +58,17 @@ unsigned int get_multi_dt_entry(unsigned int fdt_addr){
 
 		/*Get aml_dt and split to 3 strings*/
 		int i = 0;
-		unsigned char *aml_dt_buf;
-		aml_dt_buf = (unsigned char *)malloc(sizeof(unsigned char)*64);
+		char *aml_dt_buf;
+		aml_dt_buf = (char *)malloc(sizeof(char)*64);
 		memset(aml_dt_buf, 0, sizeof(aml_dt_buf));
-		unsigned char *aml_dt = getenv(AML_DT_UBOOT_ENV);
+		char *aml_dt = getenv(AML_DT_UBOOT_ENV);
 		memcpy(aml_dt_buf, aml_dt, (strlen(aml_dt)>64?64:(strlen(aml_dt)+1)));
 		unsigned int aml_dt_len = aml_dt_buf ? strlen(aml_dt_buf) : 0;
 		if(aml_dt_len <= 0){
 			printf("      Get env aml_dt failed!\n");
 			return fdt_addr;
 		}
-		unsigned char *tokens[3] = {NULL, NULL, NULL};
+		char *tokens[3] = {NULL, NULL, NULL};
 		for(i = 0; i < AML_DT_ID_VARI_TOTAL; i++){
 			tokens[i] = strsep(&aml_dt_buf, "_");
 		}
@@ -77,17 +77,17 @@ unsigned int get_multi_dt_entry(unsigned int fdt_addr){
 		printf("        aml_dt soc: %s platform: %s variant: %s\n", tokens[0], tokens[1], tokens[2]);
 
 		/*match and print result*/
-		unsigned char **dt_info;
-		dt_info = (unsigned char **)malloc(sizeof(unsigned char *)*AML_DT_ID_VARI_TOTAL);
+		char **dt_info;
+		dt_info = (char **)malloc(sizeof(char *)*AML_DT_ID_VARI_TOTAL);
 		for(i = 0; i < AML_DT_ID_VARI_TOTAL; i++)
-			dt_info[i] = (unsigned char *)malloc(sizeof(unsigned char)*aml_each_id_length);
+			dt_info[i] = (char *)malloc(sizeof(char)*aml_each_id_length);
 		unsigned int dtb_match_num = 0xffff;
-		unsigned int x = 0, y = 0; //loop counter
+		unsigned int x = 0, y = 0, z = 0; //loop counter
 		unsigned int read_data;
 		for(i = 0; i < dt_total; i++){
 			for(x = 0; x < AML_DT_ID_VARI_TOTAL; x++){
 				for(y = 0; y < aml_each_id_length; y+=4){
-					read_data = (unsigned char *)readl(fdt_addr + AML_DT_FIRST_DTB_OFFSET + \
+					read_data = readl(fdt_addr + AML_DT_FIRST_DTB_OFFSET + \
 						 i * aml_dtb_header_size + AML_DT_DTB_DT_INFO_OFFSET + \
 						 (x * aml_each_id_length) + y);
 					dt_info[x][y+0] = (read_data >> 24) & 0xff;
@@ -95,16 +95,33 @@ unsigned int get_multi_dt_entry(unsigned int fdt_addr){
 					dt_info[x][y+2] = (read_data >> 8) & 0xff;
 					dt_info[x][y+3] = (read_data >> 0) & 0xff;
 				}
+				for (z=0; z<aml_each_id_length; z++) {
+					/*fix string with \0*/
+					if (0x20 == (uint)dt_info[x][z]) {
+						dt_info[x][z] = '\0';
+					}
+				}
+				//printf("dt_info[x]: %s\n", dt_info[x]);
+				//printf("strlen(dt_info[x]): %d\n", strlen(dt_info[x]));
 			}
 			if(dt_tool_version == 1)
-				printf("        dtb %d soc: %0.4s   plat: %0.4s   vari: %0.4s\n", i, (char *)(dt_info[0]), (char *)(dt_info[1]), (char *)(dt_info[2]));
+				printf("        dtb %d soc: %.4s   plat: %.4s   vari: %.4s\n", i, (char *)(dt_info[0]), (char *)(dt_info[1]), (char *)(dt_info[2]));
 			else if(dt_tool_version == 2)
-				printf("        dtb %d soc: %0.16s   plat: %0.16s   vari: %0.16s\n", i, (char *)(dt_info[0]), (char *)(dt_info[1]), (char *)(dt_info[2]));
-			if(!strncmp(tokens[0], (char *)(dt_info[0]), strlen(tokens[0])) && \
-				!strncmp(tokens[1], (char *)(dt_info[1]), strlen(tokens[1])) && \
-				!strncmp(tokens[2], (char *)(dt_info[2]), strlen(tokens[2]))){
+				printf("        dtb %d soc: %.16s   plat: %.16s   vari: %.16s\n", i, (char *)(dt_info[0]), (char *)(dt_info[1]), (char *)(dt_info[2]));
+			uint match_str_counter = 0;
+			for (z=0; z<AML_DT_ID_VARI_TOTAL; z++) {
+				/*must match 3 strings*/
+				if (!strncmp(tokens[z], (char *)(dt_info[z]), strlen(tokens[z])) && \
+					(strlen(tokens[z]) == strlen(dt_info[z])))
+					match_str_counter++;
+			}
+			if (match_str_counter == AML_DT_ID_VARI_TOTAL) {
 				//printf("Find match dtb\n");
 				dtb_match_num = i;
+			}
+			for (z=0; z<AML_DT_ID_VARI_TOTAL; z++) {
+				/*clear data for next loop*/
+				memset(dt_info[z], 0, sizeof(aml_each_id_length));
 			}
 		}
 		/*clean malloc memory*/
@@ -127,4 +144,5 @@ unsigned int get_multi_dt_entry(unsigned int fdt_addr){
 			return fdt_addr;
 		}
 	}
+	return 0;
 }
